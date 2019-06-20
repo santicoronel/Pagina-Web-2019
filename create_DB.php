a31d6d2ab3299edc47590b43f727687f5668adfe
Santiago Coronel <santi.loga00@gmail.com>
2019-06-19 21:12:21 -0300

    Corrige sintax errors

--- create_DB.php
+++ create_DB.php
@@ -11,40 +11,40 @@ if ($conn->connect_error) {
 } 
 
 
-$sql = "CREATE DATABASE DB";
+$sql = "CREATE DATABASE PaginaWebDB";
 $conn->query($sql);
 
 $sql = "CREATE TABLE Indice (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    seccion VARCHAR(30) NOT NULL";
+    seccion VARCHAR(100) NOT NULL )";
 $conn->query($sql);
 
 $sql = "CREATE TABLE Combos_Ofertas (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    oferta VARCHAR(30) NOT NULL,
-	disponibilidad VARCHAR,
-    descripcion VARCHAR,
+    oferta VARCHAR(100) NOT NULL,
+	disponibilidad VARCHAR(100),
+    descripcion VARCHAR(100),
     precio INT(4) UNSIGNED,
-	disponible BOOLEAN DEFAULT true
-	id_items INT(4) UNSIGNED DEFAULT 0";
+	disponible BOOLEAN DEFAULT true,
+	id_items INT(4) UNSIGNED DEFAULT 0 )";
 $conn->query($sql);
 
 $sql = "CREATE TABLE Oferta_Carta (
-    id_oferta INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    id_plato INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
+    id_oferta INT(6) UNSIGNED, 
+    id_plato INT(6) UNSIGNED )";
 $conn->query($sql);
 
 $sql = "CREATE TABLE Carta (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-	plato VARCHAR(30) NOT NULL,
+	plato VARCHAR(100) NOT NULL,
 	precio INT(4) UNSIGNED,
-	disponible BOOLEAN DEFAULT true
-	id_guarnicion INT(4) UNSIGNED DEFAULT 0";
+	disponible BOOLEAN DEFAULT true,
+	id_guarnicion INT(4) UNSIGNED DEFAULT 0 )";
 $conn->query($sql);
 
 $sql = "CREATE TABLE Plato_Guarnicion (
-    id_plato INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    id_guarnicion INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY";
+    id_plato INT(6) UNSIGNED , 
+    id_guarnicion INT(6) UNSIGNED )";
 $conn->query($sql);
 
 
