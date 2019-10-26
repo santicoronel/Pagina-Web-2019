0dcb25dbd1cd36dc4ff7160f67e06781ea8f1154
Santiago Coronel <santi.loga00@gmail.com>
2019-10-26 17:56:41 -0300

    Elimina guarniciones

--- create_DB.php
+++ create_DB.php
@@ -12,12 +12,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "CREATE TABLE Indice (
+$sql = "CREATE TABLE IF NOT EXISTS Indice (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     seccion VARCHAR(100) NOT NULL )";
 $conn->query($sql);
 
-$sql = "CREATE TABLE Combos_Ofertas (
+$sql = "CREATE TABLE IF NOT EXISTS Combos_Ofertas (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     oferta VARCHAR(100) NOT NULL,
 	disponibilidad VARCHAR(100),
@@ -27,24 +27,13 @@ $sql = "CREATE TABLE Combos_Ofertas (
 	id_items INT(4) UNSIGNED DEFAULT 0 )";
 $conn->query($sql);
 
-$sql = "CREATE TABLE Oferta_Carta (
-    id_oferta INT(6) UNSIGNED, 
-    id_plato INT(6) UNSIGNED )";
-$conn->query($sql);
-
-$sql = "CREATE TABLE Carta (
+$sql = "CREATE TABLE IF NOT EXISTS Carta (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 	plato VARCHAR(100) NOT NULL,
 	precio INT(4) UNSIGNED,
-	disponible BOOLEAN DEFAULT true,
-	id_guarnicion INT(4) UNSIGNED DEFAULT 0 )";
+    tipo VARCHAR(100) NOT NULL,
+	disponible BOOLEAN DEFAULT true)";
 $conn->query($sql);
-
-$sql = "CREATE TABLE Plato_Guarnicion (
-    id_plato INT(6) UNSIGNED , 
-    id_guarnicion INT(6) UNSIGNED )";
-$conn->query($sql);
-
 
 $conn->close();
 
