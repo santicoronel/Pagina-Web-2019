a01f97e8922313244ab5b7feff0d8cb4a3bdb9d9
Santiago Coronel <santi.loga00@gmail.com>
2019-11-05 01:15:32 -0300

    Add nav de paginas

--- menuHandler.js
+++ menuHandler.js
@@ -1,25 +1,50 @@
-var Pedidos = new Array();
-var MenuItems = new Map();
-var body;
 
-document.addEventListener('load', _ => {
-    body = document.body.innerHTML;
-});
+var pedidos = {carta: new Array(), combos: new Array()};
+var body;
+var page = ['indice', 'carta', 'combos'];
+var pageContent ={
+    indice: pages.indice + components.flechaDer,
+    carta: pages.carta + components.flechaIzq + components.flechaDer,
+    combos: pages.combos + components.flechaIzq,
+};
+var pageState = 0;
 
 function selectItem (id) {
     document.getElementById(id).classList.toggle('item_on');
 }
 
-function getItems (){
+function getItems (page){
     elems = document.getElementsByClassName('item_on');
-    for(let i = 0; i < elems.length; i++) Pedidos.push(elems.item(i).id);
+    for(let i = 0; i < elems.length; i++){
+        if(page == 1) pedidos.carta.push(elems.item(i).id);
+        if(page == 2) pedidos.combos.push(elems.item(i).id);
+    }
+}
+
+function turnPage(id){
+    pageContent[pageState] = document;
+    if (id == 'Flecha-izquierda') {
+        getItems(pageState);
+        pageState--;
+    }
+    else { 
+        if(pageState == 1) getItems(pageState);
+        pageState++;
+    }
+    renderPage(page[pageState]);
 }
 
 function buildPedido(){
     body = document.body.innerHTML;
-    document.body.innerHTML = '<h1 onclick="loadBody()"> click to go back </h1>';  
+    document.body.innerHTML = '<h1 onclick="loadBody()"> click to go back </h1>';
 }
 
 function loadBody(){
     document.body.innerHTML = body;
+}
+
+function renderPage(page){
+    if(page == 'indice') renderIndice();
+    if(page == 'carta') renderCarta();
+    if(page == 'combos') renderCombos();
 }
