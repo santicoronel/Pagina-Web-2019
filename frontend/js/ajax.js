977ce0fb163e1703800c7f6c62451d3ac6127661
Santiago Coronel <santi.loga00@gmail.com>
2019-11-06 12:31:53 -0300

    Hace clase Ajax

--- frontend/js/ajax.js
+++ frontend/js/ajax.js
@@ -1,12 +1,15 @@
 
-export function fetcher(succes){
-    let xmlreq = new XMLHttpRequest();   
-    xmlreq.onreadystatechange = _ => {
-        if(this.readyState == 4 && this.status == 200){
-            let data = JSON.parse(this.responseText);
-            succes(data);
+export class Ajax {
+    
+    static fetcher(succes){
+        let xmlreq = new XMLHttpRequest();   
+        xmlreq.onreadystatechange = _ => {
+            if(this.readyState == 4 && this.status == 200){
+                let data = JSON.parse(this.responseText);
+                succes(data);
+            }
         }
+        xmlreq.open('GET', 'fetch.php', true);
+        xmlreq.send();
     }
-    xmlreq.open('GET', 'fetch.php', true);
-    xmlreq.send();
 }
