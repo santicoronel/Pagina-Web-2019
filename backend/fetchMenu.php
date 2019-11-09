370ca5c2bc9297a338b38fa455dacb2707294bb4
Santiago Coronel <santi.loga00@gmail.com>
2019-11-08 22:17:44 -0300

    Add fetchers de db en backend

---
+++ backend/fetchMenu.php
@@ -0,0 +1,15 @@
+<?php
+include './sql_variables.php';
+
+$conn = new mysqli($servername, $username, $password, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$res = $conn->query('SELECT * FROM Menu order by precio');
+$output = array();
+for($i = 0; $i < $res->num_rows; $i++) $output[$i] = $res->fetch_array(MYSQLI_ASSOC);
+
+echo json_encode($output);
+?>
